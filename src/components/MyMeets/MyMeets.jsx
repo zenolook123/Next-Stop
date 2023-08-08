93b3de257be010b7203e0e37efa1f93cb9809570
Zach Garrison <zgedina@yahoo.com>
2023-08-08 17:41:38 -0500

    my meets padding on top

--- src/components/MyMeets/MyMeets.jsx
+++ src/components/MyMeets/MyMeets.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import MyMeetCard from '../MyMeetCard/MyMeetCard';
-
+import { Container } from '@mui/material';
 
 function MyMeets() {
 
   return (
-    <div style={{margin:'0px'}}>
+    <Container>
+    <div style={{margin:'0px', paddingTop:'40px'}}>
       <h2 style={{marginLeft:'30px',marginBottom:'0px'}}>Click on "Invite" or "Edit" to see more about your meets</h2>
       <MyMeetCard />
     </div>
+    </Container>
   );
 }
 
